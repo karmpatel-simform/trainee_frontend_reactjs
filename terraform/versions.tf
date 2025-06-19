12b1172451ea93f4561c5cbc41fa5886cab9b9e8
Karm Patel <Karm.patel@simformsolutions.com>
2025-06-19 14:55:35 +0530

    Inital Commit

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -15,13 +15,11 @@ terraform {
     storage_account_name = "identitystorage12"
     container_name       = "storage1"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
 }
 
 provider "null" {
