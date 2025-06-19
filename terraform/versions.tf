5ee078d724339360c7f5d0d4e9d5df0a69e9d5f5
Karm Patel <Karm.patel@simformsolutions.com>
2025-06-19 12:00:33 +0530

    creds added

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,13 +10,14 @@ terraform {
   }
   required_version = ">=1.0"
 
-  # backend "azurerm" {
-  #   resource_group_name  = "IdentityGroup"
-  #   storage_account_name = "identitystorage12"
-  #   container_name       = "storage1"
-  #   key                  = "terraform.tfstate"
-  #   use_azuread_auth     = true
-  # }
+  backend "azurerm" {
+    resource_group_name  = "IdentityGroup"
+    storage_account_name = "identitystorage12"
+    container_name       = "storage1"
+    key                  = "terraform.tfstate"
+    tenant_id            = "your_tenant_id"
+    subscription_id      = "your_subscription_id"
+  }
 }
 
 provider "azurerm" {
