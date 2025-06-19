0dce732279368505d779c1f9092bf9c28ad962e5
Karm Patel <Karm.patel@simformsolutions.com>
2025-06-19 12:50:04 +0530

    backend creds added

---
+++ terraform/variales.tf
@@ -0,0 +1,17 @@
+# variable "client_id" {
+#   description = "Azure Client ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "subscription_id" {
+#   description = "Azure Subscription ID"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "tenant_id" {
+#   description = "Azure Tenant ID"
+#   type        = string
+#   sensitive   = true
+# }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -20,11 +20,25 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # use_oidc        = true
+  # client_id       = var.client_id
+  # subscription_id = var.subscription_id
+  # tenant_id       = var.tenant_id
 }
 
 provider "null" {
 
 }
 
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 
 
